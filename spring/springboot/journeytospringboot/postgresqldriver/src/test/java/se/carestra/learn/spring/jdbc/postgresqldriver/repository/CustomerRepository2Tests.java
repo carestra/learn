56f371aca34732c09d1225378a0a667f2eda7a96
Carlos Estrada <carlos.estrada@carestra.se>
2025-03-27 09:50:42 +0100

    Adding embedded database test support och using @TestConfiguration in CustomerRepository2Tests.

--- spring/springboot/journeytospringboot/postgresqldriver/src/test/java/se/carestra/learn/spring/jdbc/postgresqldriver/repository/CustomerRepository2Tests.java
+++ spring/springboot/journeytospringboot/postgresqldriver/src/test/java/se/carestra/learn/spring/jdbc/postgresqldriver/repository/CustomerRepository2Tests.java
@@ -1,17 +1,24 @@
 package se.carestra.learn.spring.jdbc.postgresqldriver.repository;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import se.carestra.learn.spring.jdbc.postgresqldriver.Customer;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CustomerRepository2Tests {
 
@@ -26,8 +33,26 @@ class CustomerRepository2Tests {
     List<Customer> customers = repo.findAll();
     assertNotNull(customers);
     assertThat(customers.isEmpty(), is(false));
-    assertThat(customers.size(), is(2));
+    assertThat(customers.size(), is(5));
 
     customers.forEach(System.out::println);
+  }
+
+  // Using 'spring.main.allow-bean-definition-overriding=true' to override same bean name as in 'AppConfiguration.java'.
+  @TestConfiguration
+  static class Configuration {
+    @Bean
+    public DataSource dataSource() {
+      return new EmbeddedDatabaseBuilder()
+          .generateUniqueName(true)
+          .setType(EmbeddedDatabaseType.H2)
+          .addScripts("schema.sql", "data.sql")
+          .build();
+    }
+
+    @Bean
+    public CustomerRepository customerRepository() {
+      return new CustomerDatasourceRepository(dataSource());
+    }
   }
 }
---
+++ spring/springboot/journeytospringboot/postgresqldriver/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+insert into customer(name) values ('Bella');
+insert into customer(name) values ( 'Willow');
+insert into customer(name) values ( 'Daisy');
+insert into customer(name) values ( 'Mia');
+insert into customer(name) values ( 'Ruby');
